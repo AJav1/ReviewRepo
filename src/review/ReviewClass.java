171aa9e6bf358e33dce77717400a2adba03e9641
Ahmad Javed <javed.ahmadi@tekschool>
2022-11-09 21:27:17 -0500

    test

--- src/review/ReviewClass.java
+++ src/review/ReviewClass.java
@@ -2,4 +2,12 @@ package review;
 
 public class ReviewClass {
 
+	public static void main(String[] args) {
+		
+		
+	}
+	
+	public void test() {
+		System.out.println("This is Method is on Review Session branch");
+	}
 }
